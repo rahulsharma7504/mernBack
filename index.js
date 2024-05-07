ec87e75c68684653d98969760d0e53eba25b73a2
Rahul Sharma <128781307+rahulsharma7504@users.noreply.github.com>
2024-05-07 09:39:07 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const cors=require('cors');
-const mongoose=require('mongoose');
-mongoose.connect(`${process.env.MONGO_URL}Blogs`).then(()=>{
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect(`${process.env.MONGO_URL}Blogs`).then(() => {
   console.log('Connected to MongoDB');
-})
+});
+
 const app = express();
 app.use(express.json());
-
-
 app.use(morgan('dev'));
-app.use(cors()); 
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'uploads')));
 
-app.use(express.static(path.join(__dirname, 'uploads')))
-//
+// Import the userRoute
+const userRoute = require('./Routes/UserRoutes');
 
-const UserRoutes=require('./Routes/UserRoutes');
-app.use('/',UserRoutes);
+// Mount the userRoute
+app.use('/api/users', userRoute);
 
 // For Blog Posts
-const Posts=require('./Routes/PostRoutes')
-app.use('/api/post',Posts)
+const Posts = require('./Routes/PostRoutes');
+app.use('/api/post', Posts);
 
-const PORT = process.env.PORT // Use PORT from environment variable if available, otherwise default to 3000
-
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
